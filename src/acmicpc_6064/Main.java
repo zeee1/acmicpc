2249e0948b8ad645e0e9fa4c51f177859774b1be
Min Ji-Won <miw5281@gmail.com>
2019-02-07 13:53:20 +0900

    20190207 +6064

--- src/acmicpc_6064/Main.java
+++ src/acmicpc_6064/Main.java
@@ -7,7 +7,6 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         int testcaseNum = scanner.nextInt();
-        int result = 0;
 
         for(int i = 0 ; i < testcaseNum; i++){
             int M = scanner.nextInt();
@@ -15,9 +14,73 @@ public class Main {
             int x = scanner.nextInt();
             int y = scanner.nextInt();
 
-            result = Ma+b;..
-            result = Na+b..
-            x = y
+            long maxNumber = lcm(M, N);
+
+            int copy_x = x;
+            int copy_y = y;
+            boolean answerFind = false;
+
+            if (copy_x == copy_y){
+                System.out.println(copy_x);
+                answerFind = true;
+            }
+            else if(copy_x > copy_y){
+                while(copy_x <= maxNumber){
+                    while(copy_y <= copy_x){
+                        if (copy_x == copy_y){
+                            System.out.println(copy_x);
+                            answerFind = true;
+                            break;
+                        }
+                        copy_y += N;
+                    }
+
+                    if(answerFind == true){
+                        break;
+                    }
+                    copy_x += M;
+                }
+            }
+            else if(copy_x < copy_y){
+                while(copy_y <= maxNumber){
+                    while(copy_x <= copy_y){
+                        if (copy_x == copy_y){
+                            System.out.println(copy_x);
+                            answerFind = true;
+                            break;
+                        }
+                        copy_x += M;
+                    }
+
+                    if(answerFind == true){
+                        break;
+                    }
+                    copy_y += N;
+                }
+            }
+
+
+            if(answerFind == false){
+                System.out.println(-1);
+            }
         }
+    }
+
+    public static long lcm(long a, long b) {
+        int gcd_value = gcd((int)a, (int)b);
+
+        if (gcd_value == 0)
+            return 0;
+
+        return Math.abs( (a * b) / gcd_value );
+    }
+
+    public static int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = a % b;
+            a = b;
+            b = temp;
+        }
+        return Math.abs(a);
     }
 }
