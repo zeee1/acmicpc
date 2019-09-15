1385f8ba14c2df22883e5309e6d0ef436d29bf6e
zeee1 <miw5281@gmail.com>
2019-09-15 22:17:02 +0900

    solve 10952

---
+++ src/acmicpc_10952/Main.java
@@ -0,0 +1,19 @@
+package acmicpc_10952;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        while(true){
+            int A = scanner.nextInt();
+            int B = scanner.nextInt();
+
+            if(A == 0 && B == 0)
+                break;
+
+            System.out.println(A+B);
+        }
+    }
+}
