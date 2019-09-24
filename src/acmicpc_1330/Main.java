9d1eaa8d0f4ea17087d525a0325804f991134a20
zeee1 <miw5281@gmail.com>
2019-09-24 21:40:30 +0900

    solve 1330

---
+++ src/acmicpc_1330/Main.java
@@ -0,0 +1,21 @@
+package acmicpc_1330;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+
+        if (A > B){
+            System.out.println(">");
+        }
+        else if(A < B){
+            System.out.println("<");
+        }
+        else
+            System.out.println("==");
+    }
+}
