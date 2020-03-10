64732ce94d972047df91a4a5ea587788cb13321b
zeee1 <miw5281@gmail.com>
2020-03-10 13:38:05 +0900

    solve 2579

--- src/acmicpc_2579/Main.java
+++ src/acmicpc_2579/Main.java
@@ -7,22 +7,24 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         int N = scanner.nextInt();
-        int[] stairs = new int[N+1];
+
+        int[] arr = new int[N+1];
         int[] DP = new int[N+1];
 
-        for(int i = 1; i <= N; i++){
-            stairs[i] = scanner.nextInt();
+        for(int i = 1; i <= N ;i++){
+            arr[i] = scanner.nextInt();
         }
 
-        DP[1] = stairs[1];
-        DP[2] = stairs[1]+stairs[2];
-        DP[3] = Math.max(stairs[1]+stairs[3], stairs[2]+stairs[3]);
+        DP[0] = 0;
+        DP[1] = arr[1];
 
-        for(int i = 4; i <= N; i++){
-            DP[i] = Math.max(stairs[i]+DP[i-2], stairs[i-1]+stairs[i]+DP[i-3]);
+        if(N >= 2){
+            DP[2] = arr[1]+arr[2];
+            for(int i = 3; i <= N ; i++){
+                DP[i] = Math.max(DP[i-3]+arr[i-1], DP[i-2])+arr[i];
+            }
         }
 
         System.out.println(DP[N]);
-
     }
 }
